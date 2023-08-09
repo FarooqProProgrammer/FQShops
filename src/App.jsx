d8d647312cdf3dd391605fc4f73b4628f3490a37
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-08-09 01:35:50 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Products from './pages/Products'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SingleProduct from './pages/SingleProduct'
+import About from './pages/About'
 
 export default function App() {
   return (
@@ -14,6 +15,8 @@ export default function App() {
         <Route path="/"   element={<Home />} />
         <Route path="/products"   element={<Products />} />
         <Route path="/products/:id"   element={<SingleProduct />} />
+        <Route path="/about"   element={<About />} />
+        
       </Routes>
       <Footer />
     </BrowserRouter>
